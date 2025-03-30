6880d645ddbe71bede0c891134c99b1b0399dc5c
Artur Yakimenko <ponygangster@gmail.com>
2025-03-30 03:04:51 +0300

    ITS WORK

--- ArturZZZBot/main.py
+++ ArturZZZBot/main.py
@@ -1,47 +1,61 @@
-import asyncio
-import logging
-import sys
-import API
-import requests
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from API import *
 
-TOKEN = API.botAPI
+bot = Bot(token=botAPI)
 dp = Dispatcher()
+
+class Form(StatesGroup):
+    waiting_for_text = State()
 
 
 @dp.message(CommandStart())
-async def start(message: Message) -> None:
-    await message.answer(f'Hello!')
+async def start(message: types.Message, state: FSMContext):
+    await message.answer(f'Привет, {message.from_user.first_name}, отправь мне ссылку на видео с ютуба и я скачаю и отправлю его для тебя в формате аудио!')
+    await state.set_state(Form.waiting_for_text)
 
-@dp.message(Command('youtube')) #сделать запрос ссылки и переброс на след шаг, это фейк что нижу...
-async def YT_start(message: Message) -> None:
+@dp.message(StateFilter(Form.waiting_for_text))
+async def your_handler(message: types.Message, state: FSMContext):
+    url = message.text
+    audio_path = await download_video(url)
 
-    url = "https://youtube-media-downloader.p.rapidapi.com/v2/video/details"
+    if audio_path:
+        try:
+            audio_file = FSInputFile(audio_path)
+            await message.reply_audio(audio_file, caption="Ваше аудио ♪")
+            os.remove(audio_path)
+        except Exception as e:
+            await message.reply("Ошибка отправки")
+            print(f"Ошибка: {e}")
+    else:
+        await message.reply("Не удалось скачать аудио")
 
-    querystring = {"videoId":"G33j5Qi4rE8"}
+    await state.clear()
+    await message.answer(f'Отправь еще ссылку, чтобы скачать еще видео')
+    await state.set_state(Form.waiting_for_text)
 
-    headers = {
-        "x-rapidapi-key": "831b4d7540mshf9745ac3cc801bcp103320jsn5ee0da0ded29",
-        "x-rapidapi-host": "youtube-media-downloader.p.rapidapi.com"
-    }
+def _sync_download(url):
+    try:
+        os.makedirs("downloads", exist_ok=True)
+        ydl_opts = {
+            "format": "bestaudio[ext=m4a]",
+            "outtmpl": "downloads/%(title)s.%(ext)s",
+            "quiet": True,
+            "restrictfilenames": True,
+            "no_warnings": True,
+        }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
-    video_url = data['videos']['items'][0]['url']
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            return ydl.prepare_filename(info)
 
-@dp.message()
-async def echo_message(message: Message) -> None:
-    await message.reply(f'Сорри не понял, еще раз')
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return None
+
+async def download_video(url):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, _sync_download, url)
 
 
-async def main() -> None:
-    bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    dp.run_polling(bot)
