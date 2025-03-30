319f84e0465cddc105595407bb55280fa2a925df
Artur Yakimenko <ponygangster@gmail.com>
2025-03-30 22:32:42 +0300

    full work
    i finally finish this bot

--- ArturZZZBot/main.py
+++ ArturZZZBot/main.py
@@ -9,75 +9,26 @@ class Form(StatesGroup):
 
 @dp.message(CommandStart())
 async def start(message: types.Message, state: FSMContext):
-    await message.answer(f'Привет, {message.from_user.first_name}, отправь мне ссылку на видео с ютуба и я скачаю и отправлю его для тебя в формате аудио!')
+    await message.answer(f'Привет, {message.from_user.first_name}, отправь мне ссылку на видео с ютуба и я скачаю и отправлю его для тебя в формате аудио! (.m4a)')
     await state.set_state(Form.waiting_for_text)
-
-'''
-@dp.inline_query()
-async def handle_inline_query(inline_query: types.InlineQuery):
-    query = inline_query.query.strip()
-    results = []
-
-    if not query:
-        results.extend([
-            types.InlineQueryResultArticle(
-                id='NoURL',
-                title='Insert here a video URL',
-                input_message_content=types.InputTextMessageContent(
-                    message_text=f'Insert here like @ArtursZZZBot <URL>',
-                )
-            )
-        ])
-    else:
-        url = query.strip()
-        results.append(
-            types.InlineQueryResultArticle(
-                id="YesURL",
-                title=f"AAA",
-                input_message_content=types.InputTextMessageContent(
-                    message_text=f'AAA!'
-                ),
-            )
-        )
-        try:
-            audio_path = await download_video(query)
-            if not audio_path or not os.path.exists(audio_path):
-                raise FileNotFoundError
-
-            result = InlineQueryResultAudio(
-                id="1",
-                audio_url=url,
-                title=os.path.basename(audio_path),
-            )
-
-            await inline_query.answer(results=[result], cache_time=0)
-
-        except Exception as e:
-            print(f"Ошибка: {e}")
-            await inline_query.answer(
-                results=[],
-                switch_pm_text="Ошибка загрузки",
-                switch_pm_parameter="error",
-            )
-
-        finally:
-            if audio_path and os.path.exists(audio_path):
-                os.remove(audio_path)
-'''
 
 @dp.message(StateFilter(Form.waiting_for_text))
 async def your_handler(message: types.Message, state: FSMContext):
     url = message.text
+    await message.reply(f'Начинаю скачивать...')
     audio_path = await download_video(url)
 
     if audio_path:
         try:
             audio_file = FSInputFile(audio_path)
-            await message.reply_audio(audio_file, caption="Ваше аудио ♪")
-            os.remove(audio_path)
+            await message.answer_audio(audio_file, caption="Ваше аудио ♪ \nСкачано с помощью @ArtursZZZBot")
         except Exception as e:
-            await message.reply("Ошибка отправки")
+            await message.reply("Ошибка отправки (аудиофайл превысил 50МБ)")
             print(f"Ошибка: {e}")
+            await state.set_state(Form.waiting_for_text)
+        finally:
+            await asyncio.sleep(1)
+            os.remove(audio_path)
     else:
         await message.reply("Не удалось скачать аудио")
 
@@ -92,7 +43,6 @@ def _sync_download(url):
             "format": "bestaudio[ext=m4a]",
             "outtmpl": "downloads/%(title)s.%(ext)s",
             "quiet": True,
-            "restrictfilenames": True,
             "no_warnings": True,
         }
 
